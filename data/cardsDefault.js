f57c6906f3061e44dfa112d62d6a4ae0aaa0fd7c
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-21 17:23:01 +0100

    fixed bug - used state and useEffect for cards

--- data/cardsDefault.js
+++ data/cardsDefault.js
@@ -1,27 +1,26 @@
 const cardsDefault = [
-
     {
         id: 1,
-        projectName: "Le Grand Théatre d'Al Houceima",
-        imgPath: "/projects/amphi.png",
+        projectName: "Route de l'Aéroport Al Houceima",
+        imgPath: "/projects/route.png",
+
     },
 
     {
         id: 2,
+        projectName: "Le Grand Théatre d'Al Houceima",
+        imgPath: "/projects/amphi.png",
+    },
+    
+    {
+        id: 3,
         projectName: "Piscine Batha",
         imgPath: "/projects/piscine.png",
     },
     {
-        id: 3,
+        id: 4,
         projectName: "Centre Multidisciplinaire Zouagha",
         imgPath: "/projects/centre-de-prise.png",
-    },
-
-    {
-        id: 4,
-        projectName: "Route de l'Aéroport Al Houceima",
-        imgPath: "/projects/route.png",
-
     },
     {
         id: 5,
@@ -29,7 +28,7 @@ const cardsDefault = [
         imgPath: "/projects/culture.png",
 
     },
-
+    
 ];
 
 export default cardsDefault
